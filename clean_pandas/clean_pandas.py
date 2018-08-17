b7f36cd60818c9d7749be53814cb561aafe9b878
Aaron Burgess <aaron.burgess@fssa.in.gov>
2018-08-17 15:53:14 -0400

    Added protected methods for implementing cleaning processes
    Do this so that the exposed methods can function

--- clean_pandas/clean_pandas.py
+++ clean_pandas/clean_pandas.py
@@ -1,22 +1,85 @@
-from typing import Dict, Union, List, Any
+from typing import Dict, Union, Any
+from datetime import datetime
+
 import pandas as pd
+import numpy
+
+from cryptography.fernet import Fernet
+from faker import Faker
+
+
+class UnknownCleanType(Exception):
+    """Custom Exception for unknown clean type"""
+    pass
 
 
 @pd.tools.util.pd.api.extensions.register_dataframe_accessor('clean_pandas')
 class CleanPandas:
     def __init__(self, pandas_object: Union[pd.DataFrame]):
         self._pd_obj = pandas_object
+        self._key = Fernet.generate_key()
+        self._fernet = Fernet(self._key)
+        self._faker = Faker()
+
+        def _encrypt_value(value: Any) -> bytes:
+            """
+            Take an input value, convert to bytes, and encrypt as bytes
+
+            Args:
+                value: Incoming value from Pandas Series
+
+            Returns:
+                An encrypted bytes object
+            """
+            return self._fernet.encrypt(str(value).encode())
+
+        def _fake_value(faker_type: str) -> Any:
+            """
+            Replace an input value with a fake output value of a matching type
+
+            Args:
+                faker_type: Faker provider type to use
+
+            Returns:
+                Same data type as incoming but a fake value
+            """
+            return self._faker.__dict__[faker_type]()
+
+        def _truncate_value(value: Any,
+                            dtype: Any,
+                            trunc_length: int,
+                            trunc_from_end: bool) -> Any:
+            """
+            Truncates a given value given a truncation length and direction from which to truncate
+
+            Args:
+                value: Incoming value from Pandas Series
+                dtype: Numpy object type
+                trunc_length: Length of characters to truncate
+                trunc_from_end: Boolean indicating whether to truncate from end of value or start
+
+            Returns:
+                A truncated value
+            """
+            trunc_index = len(value) - trunc_length
+            truncate_value = str(value)[:trunc_index] if trunc_from_end else str(value)[trunc_index:]
+
+            if dtype in [numpy.datetime64, numpy.object, numpy.object_]:
+                return truncate_value
+            else:
+                return dtype(truncate_value)
 
         def _create_unique_value_dict(column_name: str,
-                                      clean_type: str ='encrypt',
-                                      trunc_level: int = 0,
+                                      faker_type: str,
+                                      clean_type: str = 'encrypt',
+                                      trunc_length: int = 0,
                                       trunc_from_end=True) -> Dict[Any, Any]:
             """
             Take the unique values in a series and perform the desired cleaning operations
 
             Args:
                 clean_type: String indicating 'encrypt', 'replace', 'truncate'
-                trunc_level: Used if clean_type is 'truncate', indicates how many characters to remove
+                trunc_length: Used if clean_type is 'truncate', indicates how many characters to remove
                 trunc_from_end: Truncate from the end, will truncate from the start if False
 
             Returns:
@@ -25,5 +88,33 @@ class CleanPandas:
             replacement_xwalk_dict = {}
 
             for value in self._pd_obj[column_name].unique():
-                ## TODO: Using replacement_xwalk_dict, make the value object the key and apply the clean_type to the same object as the dictionary value
-                replacement_xwalk_dict[value] = value
+                if clean_type == 'encrypt':
+                    new_value = _encrypt_value(value)
+                elif clean_type == 'faker':
+                    new_value = _fake_value(faker_type)
+                elif clean_type == 'truncate':
+                    new_value = _truncate_value(value, self._pd_obj[column_name].dtype.type,
+                                                trunc_length, trunc_from_end)
+                else:
+                    raise UnknownCleanType("Clean type must be 'encrypt', 'faker', or 'truncate'")
+
+                replacement_xwalk_dict[value] = new_value
+
+            return replacement_xwalk_dict
+
+        def clean_series(series_name: str, clean_type: str, faker_type: str,
+                         trunc_length: int = 0, trunc_from_end: bool = True):
+            """
+            Takes the unique values in a given series, applies the clean_type and replaces all values in the
+            given series with the new "clean" values
+
+            Args:
+                series_name: Pandas series name
+                clean_type: 'encrypt', 'faker', 'truncate' are the options
+                faker_type: Faker provider type to use
+                trunc_length: Leng
+                trunc_from_end:
+
+            Returns:
+
+            """
